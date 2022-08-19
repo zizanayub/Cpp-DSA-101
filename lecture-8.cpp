ea3c2676784938df2f5239ef4944cade7fa7445c
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2022-08-19 17:53:05 +0600

    Update lecture-8.cpp

--- lecture-8.cpp
+++ lecture-8.cpp
@@ -60,3 +60,66 @@ int main()
 
 
 //OUTPUT: "Character 1" 
+
+
+
+
+
+//IF WE REMOVE DEFAULT, WHAT WILL HAPPEN?
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int n = 3;
+   switch( n )
+   {
+   case 1:
+       cout << "First" << endl;
+       break;
+   case 2:
+       cout << "Second" << endl;
+       break;
+
+       //It will print empty if we don't add 'default' case
+   }
+
+}
+
+
+//OUTPUT: 
+// ****EMPTY****
+
+
+
+////Why we added break in each case?
+//Let's find out without adding break!
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cout << "Enter the number: ";
+   cin >> n;
+
+   switch( n )
+   {
+   case 1:
+       cout << "It's 1" << endl;
+   case 2:
+       cout << "It's 2" << endl;
+   case 3:
+       cout << "It's 3" << endl;
+   }
+
+    First it will execute matched cases, then it will execute all of
+    following cases after that if break is not added!
+}
+
+
+
+//OUTPUT: Input: 2
+//"It's 2"
+//"It's 3"
