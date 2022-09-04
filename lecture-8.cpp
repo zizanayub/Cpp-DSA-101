872dfb1f622edbbf18e1344895a5a2efc42482de
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2022-09-04 22:59:15 +0600

    Update lecture-8.cpp

--- lecture-8.cpp
+++ lecture-8.cpp
@@ -531,3 +531,50 @@ int main()
 //OUTPUT
 //Input: 5
 // 1 2 3 4 5 
+
+
+
+
+
+
+
+
+
+//Check if the number is prime or not
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int checkPrime(int n)
+{
+   for ( int i = 2; i < n ; i++ )
+   {
+      if ( n % i == 0 )
+      {
+         return 0;
+      }
+   }
+   return 1; 
+}
+
+
+
+int main()
+{
+   int n;
+   cin >> n;
+   if (checkPrime(n))
+   {
+      cout << "Prime Number" << endl;
+   }
+   else 
+   {
+      cout << "Not a Prime Number."<< endl;
+   }
+}
+
+
+//OUTPUT
+//3
+//Prime Number 
