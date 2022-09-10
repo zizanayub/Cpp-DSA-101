25f3d313afb5b4ecc03c3d59c9867683f85293e3
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2022-09-10 16:16:15 +0600

    Create lecture-9.cpp

---
+++ lecture-9.cpp
@@ -0,0 +1,54 @@
+//Array
+//Filling with 0 in array
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int array[5] = {1};
+    cout << array[1];
+}
+
+
+//OUTPUT
+//0
+
+//******Array automatically fills out the index with 0 if the values in the array are not declared.
+
+
+
+
+
+
+
+
+//Homework-1
+//Fill out the whole array[1000] with 1 (or any number)
+//Solve of Homework
+//Comment of Kartik Gupta
+//*****fill_n command 
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int array[100];
+    fill_n(array,100,-9);
+
+    for ( int i = 1; i <= 5; i++ )
+    {
+        cout << array[i] << endl;
+    }
+}
+
+//OUTPUT
+// -9
+// -9
+// -9
+// -9
+// -9
