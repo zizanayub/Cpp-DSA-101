2fbf6c486b4c9631c5c56509db1ad60fe27bb294
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2022-09-14 23:44:19 +0600

    Update lecture-9.cpp

--- lecture-9.cpp
+++ lecture-9.cpp
@@ -52,3 +52,81 @@ int main()
 // -9
 // -9
 // -9
+
+
+
+
+
+
+
+
+
+//Array with Functions
+#include<bits/stdc++.h>
+using namespace std;
+
+void printArray(int arr[],int size)
+{
+    cout << "Printing the Array" << endl;
+    for (int i = 0; i <= size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+}
+
+
+int main()
+{
+    int array[10] = {1,4,21,2};
+    printArray(array,10);
+}
+
+
+//OUTPUT
+// Printing the Array
+// 1 4 21 2 0 0 0 0 0 0 16
+
+
+
+
+
+
+
+
+
+
+//sizeof
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int array[12] = {1,2,31,21};
+    cout << "The size of the array is: " << sizeof(array)/sizeof(int) <<endl;
+
+}
+//OUTPUT
+
+
+
+
+
+
+
+
+
+//Different types of array
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char chArray[15] = {'a','e','b','z'};
+    for (int i = 0; i <= 20; i++)
+    {
+        cout << chArray[i] << " ";
+    }
+}
+//OUTPUT
+//a e b z                       ☼       0 ↨
